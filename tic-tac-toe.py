4928fb8b8df8038f1b86d3a90381ba3d8920ab94
joaomiranda04 <159723134+joaomiranda04@users.noreply.github.com>
2024-02-12 00:04:04 +0100

    added board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,23 @@
 # tic-tac-toe 
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# print the game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+
+# take player input
+
+# check for win or tie 
+
+# switch the player 
+
+# check for win or tie again
